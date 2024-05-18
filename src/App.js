f49efe35087e01af0ab4daef83e2de6b618f2a0f
OlegKryvohub <2103kokakola2004@gmail.com>
2024-05-18 15:57:01 +0300

    ssиdfffrd

--- src/App.js
+++ src/App.js
@@ -8,117 +8,117 @@ function App() {
         <div className="menu-item">
           <p>Цибулеві кільця</p>
           <p></p>
-          <p>60 грн \ 10 шт</p>
+          <p>60 грн / 10 шт</p>
         </div>
         <div className="menu-item">
           <p>Нагетси</p>
           <p></p>
-          <p>70 грн \ 8 шт</p>
+          <p>70 грн / 8 шт</p>
         </div>
         <div className="menu-item">
           <p>Ковбаски гриль</p>
           <p></p>
-          <p>60 грн \ 100 г</p>
+          <p>60 грн / 100 г</p>
         </div>
         <div className="menu-item">
           <p>Картопля по селянськи</p>
           <p></p>
-          <p>50 грн \ 300 г</p>
+          <p>50 грн / 300 г</p>
         </div>
         <div className="menu-item">
           <p>Картопля Фрі</p>
           <p></p>
-          <p>65 грн \ 200 г</p>
+          <p>65 грн / 200 г</p>
         </div>
         <div className="menu-item">
           <p>Cоус</p>
           <p></p>
-          <p>20 грн \ 1 шт</p>
+          <p>20 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Салат літний</p>
           <p></p>
-          <p>50 грн \ порція</p>
+          <p>50 грн / порція</p>
         </div>
         <div className="menu-item">
           <p>Салат грецький</p>
           <p></p>
-          <p>110 грн \ 250 г</p>
+          <p>110 грн / 250 г</p>
         </div>
         <div className="menu-item">
           <p>Салат цезар</p>
           <p></p>
-          <p>130 грн \ 250 г</p>
+          <p>130 грн / 250 г</p>
         </div>
         <div className="menu-item">
           <p>Печінка з цибулькою</p>
           <p></p>
-          <p>75 грн \ 250 г</p>
+          <p>75 грн / 250 г</p>
         </div>
         <div className="menu-item">
           <p>Відбивна куряча</p>
           <p></p>
-          <p>60 грн \ 1 шт</p>
+          <p>60 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Відбивна свинна</p>
           <p></p>
-          <p>60 грн \ 1 шт</p>
+          <p>60 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек класичний</p>
           <p></p>
-          <p>70 грн \ 1 шт</p>
+          <p>70 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек класичний міні</p>
           <p></p>
-          <p>50 грн \ 1 шт</p>
+          <p>50 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек м'ясо+сир</p>
           <p></p>
-          <p>75 грн \ 1 шт</p>
+          <p>75 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек м'ясо+сир міні</p>
           <p></p>
-          <p>60 грн \ 1 шт</p>
+          <p>60 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек м'ясо+сир+чилі</p>
           <p></p>
-          <p>75 грн \ 1 шт</p>
+          <p>75 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Чебурек м'ясо+сир+чилі міні</p>
           <p></p>
-          <p>60 грн \ 1 шт</p>
+          <p>60 грн / 1 шт</p>
         </div>
         <div className="menu-item">
           <p>Вареники з картоплею та шкварками </p>
           <p></p>
-          <p> 60 грн \ порція</p>
+          <p> 60 грн / порція</p>
         </div>
         <div className="menu-item">
           <p>Вареники з картоплею та сиром</p>
           <p></p>
-          <p>60 грн \ порція</p>
+          <p>60 грн / порція</p>
         </div>
         <div className="menu-item">
           <p>Пельмені</p>
           <p></p>
-          <p>70 грн \ порція</p>
+          <p>70 грн / порція</p>
         </div>
         <div className="menu-item">
           <p>Деруни зі сметаною</p>
           <p></p>
-          <p>70 грн \ порція</p>
+          <p>70 грн / порція</p>
         </div>
         <div className="menu-item">
           <p>Дерун козацький</p>
           <p></p>
-          <p>120 грн \ порція</p>
+          <p>120 грн / порція</p>
         </div>
       </div>
     </div>
