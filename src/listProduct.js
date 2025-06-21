d3a055ca84fc2a49ecdd9081cf4258d140d67142
OlegKryvohub <2103kokakola2004@gmail.com>
2025-06-21 14:34:39 +0300

    new price

--- src/listProduct.js
+++ src/listProduct.js
@@ -19,13 +19,19 @@ export const listProduct = {
             img: "cheesy.png"
         },
         {
-            title: "Барбекю",
+            title: "Часниковий",
             price: 20,
             number: "1 шт",
-            img: "Barbecue.png"
+            img: "tartare.png"
         },
         {
             title: "Чилі",
+            price: 20,
+            number: "1 шт",
+            img: "chili.png"
+        },
+        {
+            title: "Кисло солодкий",
             price: 20,
             number: "1 шт",
             img: "chili.png"
@@ -34,19 +40,19 @@ export const listProduct = {
     salads : [
         {
             title: "Салат літній",
-            price: 50,
+            price: 90,
             number: "Порція",
             img: "summer.png"
         },
         {
             title: "Салат грецький",
-            price: 110,
+            price: 120,
             number: "250 г",
             img: "greek.png"
         },
         {
             title: "Салат цезар",
-            price: 130,
+            price: 150,
             number: "250 г",
             img: "cheesy.png"
         },
@@ -54,37 +60,37 @@ export const listProduct = {
     beer : [
         {
             title: "Микулин 900",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "900.png"
         },
         {
             title: "Вайсбург темний",
-            price: 80,
+            price: 120,
             number: "1 л",
             img: "black.png"
         },
         {
             title: "Тернове поле",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "ternove.png"
         },
         {
             title: "Душа пивовара",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "sould.png"
         },
         {
             title: "Микулин Медове",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "honey.png"
         },
         {
             title: "Сидр “Кров москаля”",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "blood.png"
         },
@@ -96,106 +102,106 @@ export const listProduct = {
         },
         {
             title: "Сидр Грейпфрут",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "grapefruit.png"
         },
         {
             title: "Крафтове",
-            price: 100,
+            price: 120,
             number: "1 л",
             img: "craft.png"
         },
         {
             title: "Ель Медуза",
-            price: 80,
+            price: 100,
             number: "1 л",
             img: "jellyfish.png"
         },
      
     ],
     mainCourse : [
-        {
-            title: "Печінка з цибулькою",
-            price: 75,
-            number: "250 г",
-            img: "1.png"
-        },
+        // {
+        //     title: "Печінка з цибулькою",
+        //     price: 75,
+        //     number: "250 г",
+        //     img: "1.png"
+        // },
         {
             title: "Відбивна куряча",
-            price: 60,
+            price: 80,
             number: "1 шт",
             img: "2.png"
         },
         {
             title: "Відбивна свинна",
-            price: 60,
+            price: 80,
             number: "1 шт",
             img: "3.png"
         },
         {
             title: "Чебурек класичний",
-            price: 70,
+            price: 100,
             number: "1 шт",
             img: "4.png"
         },
         {
             title: "Чебурек класичний міні",
-            price: 50,
+            price: 70,
             number: "1 шт",
             img: "5.png"
         },
         {
             title: "Чебурек м’ясо + сир",
-            price: 75,
+            price: 100,
             number: "1 шт",
             img: "6.png"
         },
         {
             title: "Чебурек м’ясо + сир міні",
-            price: 60,
+            price: 70,
             number: "1 шт",
             img: "7.png"
         },
         {
             title: "Чебурек м’ясо + сир + чилі",
-            price: 75,
+            price: 100,
             number: "1 шт",
             img: "8.png"
         },
         {
             title: "Чебурек м’ясо + сир + чилі міні",
-            price: 60,
+            price: 70,
             number: "1 шт",
             img: "9.png"
         },
         {
             title: "Вареники з картоплею і сиром",
-            price: 60,
+            price: 100,
             number: "Порція",
             img: "10.png"
         },
         {
             title: "Вареники з картоплею і шкварками",
-            price: 60,
+            price: 100,
             number: "Порція",
             img: "11.png"
         },
         {
             title: "Пельмені",
-            price: 70,
+            price: 110,
             number: "Порція",
             img: "12.png"
         },
         {
             title: "Деруни зі сметаною",
-            price: 70,
+            price: 100,
             number: "Порція",
             img: "13.png"
         },
         {
             title: "Дерун козацький",
-            price: 120,
+            price: 150,
             number: "Порція",
             img: "14.png"
         },
@@ -203,43 +209,43 @@ export const listProduct = {
     tomatoBase : [
         {
             title: "Маргарита",
-            price: 160,
+            price: 200,
             number: "1 шт",
             img: "1.png"
         },
         {
             title: "Вегетаріана",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "2.png"
         },
         {
             title: "Афіна",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "3.png"
         },
         {
             title: "Неаполітана",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "4.png"
         },
         {
             title: "М’ясна",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "5.png"
         },
         {
             title: "Салямі",
-            price: 180,
+            price: 230,
             number: "1 шт",
             img: "6.png"
         },
         {
             title: "Тоскана",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "7.png"
         },
@@ -251,13 +257,13 @@ export const listProduct = {
         // },
         {
             title: "Провінціале",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "9.png"
         },
         {
             title: "Марінара",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "10.png"
         },
@@ -265,43 +271,43 @@ export const listProduct = {
     creamBase : [
         {
             title: "Капрічоза",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "1.png"
         },
         {
             title: "Гавайська",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "2.png"
         },
         {
             title: "Палерма",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "3.png"
         },
         {
             title: "Верона",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "5.png"
         },
         {
             title: "Флоренція",
-            price: 200,
+            price: 250,
             number: "1 шт",
             img: "6.png"
         },
         {
             title: "Венеціала",
-            price: 180,
+            price: 250,
             number: "1 шт",
             img: "7.png"
         },
         {
             title: "4 сири",
-            price: 240,
+            price: 280,
             number: "1 шт",
             img: "8.png"
         },
@@ -309,7 +315,7 @@ export const listProduct = {
     appetizer : [
         {
             title: "Цибулеві кільця",
-            price: 60,
+            price: 100,
             number: "10 шт",
             img: "1.png"
         },
@@ -321,19 +327,19 @@ export const listProduct = {
         },
         {
             title: "Нагетси",
-            price: 70,
+            price: 100,
             number: "8 шт",
             img: "2.png"
         },
         {
             title: "Картопля по селянськи",
-            price: 50,
+            price: 80,
             number: "300 г",
             img: "4.png"
         },
         {
             title: "Картопля Фрі",
-            price: 65,
+            price: 80,
             number: "200 г",
             img: "5.png"
         },
@@ -345,39 +351,39 @@ export const listProduct = {
         },
         {
             title: "Овочева нарізка",
-            price: 90,
+            price: 120,
             number: "300 г",
             img: "8.png"
         },
         {
             title: "Сирна нарізка",
-            price: 450,
+            price: 350,
             number: "450 г",
             img: "7.png"
         },
         {
             title: "Ковбаски гриль",
-            price: 600,
+            price: 700,
             number: "Кг",
             img: "9.png"
         },
         {
             title: "Ребра медові",
-            price: 600,
+            price: 700,
             number: "Кг",
             img: "11.png"
         },
         {
             title: "Шашлик",
-            price: 800,
+            price: 1000,
             number: "Кг",
             img: "12.png"
         },
-        {
-            title: "Картопляні крокети",
-            price: 80,
-            number: "Порція",
-            img: "10.png"
-        },
+        // {
+        //     title: "Картопляні крокети",
+        //     price: 80,
+        //     number: "Порція",
+        //     img: "10.png"
+        // },
     ],
 }
